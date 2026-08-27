4c7bccb936ecbde281fcf493ae405a5b11a71a7e
oliveirabilu <carloscesaroliveira13@hotmail.com>
2026-08-27 11:35:58 -0300

    chore: cria projeto base com Spring Initializr

---
+++ src/main/java/com/carlos/jwtapi/JwtApiApplication.java
@@ -0,0 +1,13 @@
+package com.carlos.jwtapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtApiApplication.class, args);
+	}
+
+}
